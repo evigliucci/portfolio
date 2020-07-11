6263e698e376100a45d9212f910aa8d66564a9cb
Erika Vigliucci <evigliucci@gmail.com>
2020-07-11 16:45:12 -0400

    build xfinity site

--- portfolio/src/pages/projects/xfinity.js
+++ portfolio/src/pages/projects/xfinity.js
@@ -7,40 +7,23 @@ import Image from "../../components/image";
 const Progressive = () => {
   return (
     <section className="progressive">
-      <TitleBar title="leakSmart" />
+      <TitleBar title="Xfinity" />
       <div className="project">
         <ProjectBar
           company="GO2"
-          client="Waxman"
-          year="2015-2016"
-          tech={[
-            "Shopify Eccomerce",
-            "Wordpress",
-            "HTML5",
-            "Semantic HTML",
-            "CSS3",
-            "SCSS",
-            "Responsive Design"
-          ]}
+          client="Comcast"
+          year="2016"
+          tech={["HTML5", "Semantic HTML", "CSS3", "SCSS", "Responsive Design"]}
         />
         <div className="projectContent grid no-pop">
           <div className="col-12">
-            <Image
-              path="leaksmart/leaksmart-phone.png"
-              alt="Leaksmart Phone Image"
-            />
+            <Image path="xfinity/xfinity-laptops.png" alt="Xfinity Image" />
           </div>
-          <div className="col-12">
-            <div className="blurb">
-              <p>
-                Progressive.com’s Homepage was built as a mobile first adaptive
-                design. It was unique for performance and SEO reasons.
-              </p>
-            </div>
-            <Image
-              path="leaksmart/leaksmart-home.png"
-              alt="Progressive Home Image"
-            />
+          <div className="col-6">
+            <Image path="xfinity/xfinity-red.png" alt="Xfinity Image" />
+          </div>
+          <div className="col-6">
+            <Image path="xfinity/xfinity-blue.png" alt="Xfinity Image" />
           </div>
         </div>
       </div>
