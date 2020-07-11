4cd84455e74f93837375890d1ef9e41d19a0b766
Erika Vigliucci <evigliucci@gmail.com>
2020-07-11 16:42:06 -0400

    build first national bank page

--- portfolio/src/pages/projects/first-national-bank.js
+++ portfolio/src/pages/projects/first-national-bank.js
@@ -7,14 +7,13 @@ import Image from "../../components/image";
 const Progressive = () => {
   return (
     <section className="progressive">
-      <TitleBar title="leakSmart" />
+      <TitleBar title="First National Bank" />
       <div className="project">
         <ProjectBar
           company="GO2"
-          client="Waxman"
-          year="2015-2016"
+          client="First National Bank"
+          year="2017-2018"
           tech={[
-            "Shopify Eccomerce",
             "Wordpress",
             "HTML5",
             "Semantic HTML",
@@ -23,23 +22,41 @@ const Progressive = () => {
             "Responsive Design"
           ]}
         />
-        <div className="projectContent grid no-pop">
+        <div className="projectContent grid">
           <div className="col-12">
             <Image
-              path="leaksmart/leaksmart-phone.png"
-              alt="Leaksmart Phone Image"
+              path="first-national/fnb-blog.png"
+              alt="First National Bank Image"
             />
           </div>
-          <div className="col-12">
-            <div className="blurb">
-              <p>
-                Progressive.com’s Homepage was built as a mobile first adaptive
-                design. It was unique for performance and SEO reasons.
-              </p>
-            </div>
+          <div className="col-6">
             <Image
-              path="leaksmart/leaksmart-home.png"
-              alt="Progressive Home Image"
+              path="first-national/fnb-blog-small.png"
+              alt="First National Bank Image"
+            />
+          </div>
+          <div className="col-6">
+            <Image
+              path="first-national/fnb-blog-post.png"
+              alt="First National Bank Image"
+            />
+          </div>
+          <div className="col-8" data-push-left="off-2">
+            <Image
+              path="first-national/fnb-menu.png"
+              alt="First National Bank Image"
+            />
+          </div>
+          <div className="col-6">
+            <Image
+              path="first-national/fnb-contact.png"
+              alt="First National Bank Image"
+            />
+          </div>
+          <div className="col-5" data-push-left="off-1">
+            <Image
+              path="first-national/fnb-email.png"
+              alt="First National Bank Image"
             />
           </div>
         </div>
