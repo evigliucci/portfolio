1fe0c8aecbdaf3722864ca51a8139396410958cc
Erika Vigliucci <evigliucci@gmail.com>
2020-07-15 21:32:23 -0400

    add link fix

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -6,19 +6,19 @@ const Footer = props => {
     <footer className="footer">
       <h2 className="heading-1">Contact Me</h2>
       <div className="footerWrap">
-        <a href="https://github.com/evigliucci" target="_blank">
+        <a href="https://github.com/evigliucci" target="_blank" rel="noopener noreferrer">
           <img
             src={require("../assets/images/icon-github.png")}
             alt="Connect on Github"
           />
         </a>
-        <a href="https://www.linkedin.com/in/erika-vigliucci/" target="_blank">
+        <a href="https://www.linkedin.com/in/erika-vigliucci/" target="_blank" rel="noopener noreferrer">
           <img
             src={require("../assets/images/icon-linkedin.png")}
             alt="Connect on LinkedIn"
           />
         </a>
-        <a href="mailto:evigliucci@gmail.com" target="_blank">
+        <a href="mailto:evigliucci@gmail.com" target="_blank" rel="noopener noreferrer">
           <img
             src={require("../assets/images/icon-email.png")}
             alt="Connect via Email"
